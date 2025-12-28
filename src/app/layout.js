2235c438b5e1120f80b0b62d007816e301de5e75
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-28 14:26:16 +0000

    adding new components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import NavBar from '@/components/navbar/NavBar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +16,9 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className='container'>
           <div className='wrapper'>
+            <NavBar/>
              {children}
+             <Footer/>
             </div>
            </div>
       </body>
