59a94923e46ea4fd1d1d6e188e744af5c2a74519
Raghav Mittal <mittalraghav45@gmail.com>
2025-12-28 01:07:31 +0000

    firts commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,13 @@
+import NavBar from "@/components/navbar/NavBar";
 import styles from "./homepage.module.css";
+import Link from "next/link";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
 }
